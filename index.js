c8798d4042dc9c4a4131f2797898761184b87fe3
Tanner Foster <tannerfoster@ferristexas.gov>
2025-02-16 00:21:54 -0600

    Lab Merge Users Submission

--- index.js
+++ index.js
@@ -1,4 +1,23 @@
+require('datejs');
 
+
+function combineUsers (...args) {
+
+  const todate = Date.today();
+
+
+
+  const combinedObject = {
+    users: [],
+    merge_date: todate.toString("M/d/yyyy"),
+  };
+
+  args.forEach(element => {
+    combinedObject.users.push(...args)
+  })
+
+  return combinedObject;
+}
 
 
 module.exports = {
